ad4446025d331ffd30a7aee6f79e8a579ea8ccd9
Tiyenti <11019859+Tiyenti@users.noreply.github.com>
2023-04-18 00:14:47 +0100

    working proof of concept

---
+++ ladderdata.js
@@ -0,0 +1,11 @@
+var ladderdata = 
+{
+    // No WB Players
+    "76561198079410025": "nowb",
+
+    // WB Players
+    "76561198054382627": "wb",
+
+    // Banned Players
+    "76561198212480060": "banned",
+}
