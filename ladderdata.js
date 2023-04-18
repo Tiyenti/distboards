a0b61a40c01d6351f1277aa740c6bb1bd0de03a2
Tiyenti <11019859+Tiyenti@users.noreply.github.com>
2023-04-18 02:07:44 +0100

    implement support for map-specific exceptions (and populated ladderdata with them) kind of "dumb" right now because i have to manually remove exceptions atm but i guess i can change that later if i feel like going through the boring process of updating that giant table again

--- ladderdata.js
+++ ladderdata.js
@@ -1,13 +1,450 @@
 var ladderdata = 
 {
     // No WB Players
-    "76561198079410025": "nowb",
+    "76561198079410025":
+    {
+        "default": "nowb",
+        "exceptions": {}
+    },
 
     // WB Players
-    "76561198054382627": "wb",
+    "76561198054382627":
+    {
+        "default": "wb",
+        "exceptions": {}
+    },
+
+    "76561198158990613":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Inferno": "wb",
+        },
+    },
+
+    "76561198147688475":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Inferno": "wb",
+            "Ascension": "wb",
+            "Brink": "wb",
+            "Broken Symmetry": "wb",
+        },
+    },
+
+    "76561198129061896":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Inferno": "wb",
+        },
+    },
+
+    "76561198129061896":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Inferno": "wb",
+            "Ascension": "wb",
+        },
+    },
+
+    "76561198025607548": 
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Inferno": "wb",
+        },
+    },
+
+    "76561198001728979":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Inferno": "wb",
+            "Fulcrum": "wb",
+            "SR Motorplex": "wb",
+        },
+    },
+
+    "76561198061615921":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Inferno": "wb",
+            "Vector Valley": "wb",
+            "Micro": "wb",
+            "Outrun": "wb",
+            "Overdrive": "wb",
+            "Static Fire Signal": "wb",
+            "Uncanny Valley": "wb",
+            "Whisper": "wb",
+            "Forsaken Shrine": "wb",
+            "Candles of Hekate": "wb",
+            "Affect": "wb",
+        },
+    },
+
+    "76561198045059228":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Ascension": "wb",
+        },
+    },
+
+    "76561198035133380":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Aftermath": "wb",
+            "Iris": "wb",
+            "Pulse of a Violent Heart": "wb",
+            "Rooftops": "wb",
+            "Vector Valley": "wb",
+        },
+    },
+
+    "76561197975071752":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Affect": "wb",
+            "Aftermath": "wb",
+            "Escape": "wb",
+            "Falling Through": "wb",
+            "Inferno": "wb",
+            "Resonance": "wb",
+            "Vector Valley": "wb",
+        },
+    },
+
+    "76561199003029797": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Vector Valley": "wb",
+        },
+    },
+
+    "76561198154743494": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Escape": "wb",
+            "Vector Valley": "wb",
+        },
+    },
+
+    "76561198062748732":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Ascension": "wb",
+            "Pulse of a Violent Heart": "wb",
+            "Shadow of the Beast": "wb",
+        },
+    },
+
+    "76561198036098632": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "It Was Supposed To Be Perfect": "wb",
+            "Pulse of a Violent Heart": "wb",
+            "Shadow of the Beast": "wb",
+            "Sword": "wb",
+            "Vector Valley": "wb",
+        },
+    },
+
+    "76561198799287354": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Tetreal": "wb",
+        },
+    },
+
+    "76561198094835423": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Ascension": "wb",
+        },
+    },
+
+    "76561198084909444": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Escape": "wb",
+        },
+    },
+
+    "76561198139613473": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Vector Valley": "wb",
+        },
+    },
+
+    "76561198040630941": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Entanglement": "wb",
+            "Knowledge": "wb",
+        },
+    },
+
+    "76561198231323596": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Sea": "wb",
+        },
+    },
+
+    "76561198005890108": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Ascension": "wb",
+            "Rooftops": "wb",
+        },
+    },
+
+    "76561198119877475": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Vector Valley": "wb",
+        },
+    },
+
+    "76561198089234092": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Broken Symmetry": "wb",
+        },
+    },
+
+    "76561198059197288": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Inferno": "wb",
+        },
+    },
+
+    "76561198010296669": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Approach": "wb",
+            "Entanglement": "wb",
+            "Outrun": "wb",
+        },
+    },
+
+    "76561199071442097": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Rooftops": "wb",
+        },
+    },
+
+    "76561198020318254": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Escape": "wb",
+        },
+    },
+
+    "76561198092130149": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Vector Valley": "wb",
+        },
+    },
+
+    "76561198026317880": {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Ascension": "wb",
+        },
+    },
+
+    "76561197989148922":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Broken Symmetry": "wb",
+        },
+    },
+
+    "76561198023567939":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Inferno": "wb",
+        },
+    },
+
+    "76561198088686096":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Vector Valley": "wb",
+        },
+    },
+
+    "76561198018533542":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Vibe": "wb",
+        },
+    },
+
+    "76561198044723799":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Outrun": "wb",
+            "Vector Valley": "wb",
+        },
+    },
+
+    "76561198169816693":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Pulse of a Violent Heart": "wb",
+        },
+    },
+
+    "76561198218382311":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Vector Valley": "wb",
+        },
+    },
+
+    "76561198262160021":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Sea": "wb",
+        },
+    },
+
+    "76561198266321321":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Inferno": "wb",
+        },
+    },
+
+    "76561198008412100":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Ascension": "wb",
+        },
+    },
+
+    "76561198064267450":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Vector Valley": "wb",
+        },
+    },
+
+    "76561198404446933":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Outrun": "wb",
+        },
+    },
+
+    "76561198170039661":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Inferno": "wb",
+            "Wired": "wb",
+        },
+    },
+
+    "76561197975665382":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Aftermath": "wb",
+        },
+    },
+
+    "76561198047147439":
+    {
+        "default": "unassigned",
+        "exceptions":
+        {
+            "Inferno": "wb",
+        },
+    },
 
     // Banned Players
-    "76561198212480060": "banned",
-    "76561198559391102": "banned",
-    "76561198128379032": "banned",
+    "76561198212480060":
+    {
+        "default": "banned",
+        "exceptions": {}
+    },
+    "76561198559391102":
+    {
+        "default": "banned",
+        "exceptions": {}
+    },
+    "76561198128379032":
+    {
+        "default": "banned",
+        "exceptions": {}
+    }
 }
